f9a97d73a3f1f8e023b59c17d9015c964f0fbd6e
Qwerty042 <28468486+Qwerty042@users.noreply.github.com>
2026-01-31 18:04:36 +1300

    fighting background and fix luchador prefab

---
+++ Assets/Scripts/GlobalGameState.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class GlobalGameState
+{
+    public const float MAX_PLAYER_HEALTH = 100.0f;
+    public static float playerHealth;
+    public static string nextLuchador;
+    public static int prevLuchador;
+    public static int playerScore;
+}
